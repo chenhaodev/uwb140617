c7fc91f004bbebf1eda34a57c82ef56af303cd32
dreamclinger <chenhaomails@gmail.com>
2014-06-18 22:13:54 +0800

    test1 start

--- test.py
+++ test.py
@@ -8,14 +8,18 @@ import sys
 #lines=f.readlines()
 #fn=open(nfilename, "w")
 
-tag_x = 1;
-tag_y = 1;
-EbNo = -15;
-pulse_order = 1;
-debug = 1;
-
-for i in range(0,2):
-  cmd1="./main.sh "+str(tag_x)+" "+str(tag_y)+" "+str(EbNo)+" "+str(pulse_order)+" "+str(debug)
+for j in range(0,5):
+  tag_x = 1;
+  tag_y = 1;
+  EbNo = -15;
+  pulse_order = 1;
+  debug = 1;
+  cmd1= "echo "+ "\""+ "#****** bgn loop:" + str(j) + " *******#" +"\""+ " >> log"
   os.system(cmd1)
-  tag_x = tag_x + 1;
-  tag_y = tag_y + 1;
+  for i in range(0,9):
+    cmd2="./main.sh "+str(tag_x)+" "+str(tag_y)+" "+str(EbNo)+" "+str(pulse_order)+" "+str(debug)
+    os.system(cmd2)
+    tag_x = tag_x + 1;
+    tag_y = tag_y + 1;
+  cmd3= "echo "+ "\""+ "#****** end loop:" + str(j) + " *******#" +"\""+ " >> log"
+  os.system(cmd3)
