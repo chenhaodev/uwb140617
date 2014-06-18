01e331a847a3122f461d66f0297dabe001b0f47a
dreamclinger <chenhaomails@gmail.com>
2014-06-18 21:50:40 +0800

    -

--- main.sh
+++ main.sh
@@ -5,5 +5,7 @@ EbNo=$3
 pulse_order=$4
 debug=$5
  
-~/.octave38/bin/octave -qf maintest.m $tag_x $tag_y $EbNo $pulse_order $debug
-
+echo "# Tag position: [$tag_x $tag_y]" >> log
+echo "# EbNo = $EbNo dB"   >> log
+~/.octave38/bin/octave -qf maintest.m $tag_x $tag_y $EbNo $pulse_order $debug >> log
+echo "generated ...done "   >> log
--- test.py
+++ test.py
@@ -15,11 +15,7 @@ pulse_order = 1;
 debug = 1;
 
 for i in range(0,2):
-  #+" "+str(EbNo)+" "+str(pulse_order)+" "+str(debug)
-  cmd1="echo " + "tag:["+str(tag_x)+" "+str(tag_y)+"]"+ " EbNo = " + str(EbNo)  +" >> log"
+  cmd1="./main.sh "+str(tag_x)+" "+str(tag_y)+" "+str(EbNo)+" "+str(pulse_order)+" "+str(debug)
   os.system(cmd1)
-  #print "tag:["+str(tag_x)+" "+str(tag_y)+"]" >> log 
-  cmd2="./main.sh "+str(tag_x)+" "+str(tag_y)+" "+str(EbNo)+" "+str(pulse_order)+" "+str(debug)+" >> log"
-  os.system(cmd2)
   tag_x = tag_x + 1;
   tag_y = tag_y + 1;
